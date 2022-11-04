c24457f7b93a77621dbdb0d73b0ae65c6635bbf5
Mime <74558118+Mimexe@users.noreply.github.com>
2022-11-04 13:49:36 +0100

    Update ninit.cmd

--- ninit.cmd
+++ ninit.cmd
@@ -27,6 +27,7 @@ start /wait cmd /c "git commit -a -m 'Initial'"
 start /wait cmd /c "git branch -M main"
 start /wait cmd /c "git push -u origin main"
 start /wait cmd /c "git push --set-upstream origin main"
+start /wait cmd /c "git branch --set-upstream-to=origin/main main"
 echo Opening VSCODE
 start /wait cmd /c "code ."
 echo Initialized!
