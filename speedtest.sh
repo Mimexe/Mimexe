62f006094b372ab5b62f12f3a5b5b37cdda8607d
Mime <74558118+Mimexe@users.noreply.github.com>
2023-07-18 20:57:36 +0200

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,10 +1,12 @@
-sudo apt-get update
-sudo apt-get install curl
-curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-sudo apt-get install speedtest
+echo Installing and running speedtest...
+sudo apt-get update > /dev/null
+sudo apt-get install curl > /dev/null
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash > /dev/null
+sudo apt-get install speedtest > /dev/null
 speedtest > /tmp/speedtest.log
-sudo apt-get remove speedtest
+sudo apt-get remove speedtest -y > /dev/null
 sudo rm /etc/apt/sources.list.d/ookla_speedtest-cli.list
 sudo rm /etc/apt/keyrings/speedtest-cli-archive-keyring.gpg
+clear
 cat /tmp/speedtest.log
 rm /tmp/speedtest.log
